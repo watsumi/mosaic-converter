8921da1a15e9750f48f279d2c423677e915ce695
Wataru Utsumi <67275879+watsumi@users.noreply.github.com>
2025-02-05 01:09:56 +0900

    biome

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
-import { createRoot } from 'react-dom/client';
-import { Main } from './main';
-import './index.css'
+import { createRoot } from "react-dom/client";
+import { Main } from "./main";
+import "./index.css";
 
-
-const container = document.querySelector('#root') as Element;
+const container = document.querySelector("#root") as Element;
 const root = createRoot(container);
 
 root.render(<Main />);
